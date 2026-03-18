838856bd458548cfa525316f204b9a3ee9d3dfb0
OrdazMaestre <ordazmaestre@gmail.com>
2026-03-18 17:48:56 +0100

    COLORES_COMPLETADOS

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,29 @@
-function boxColor ({color, value}) {
+import { useState, useEffect, useRef } from 'react';
+
+function BoxColor({ color, value }) {
+  const boxRef = useRef(null);
+  const [classList, setClassList] = useState('');
+
+  const boxCSS = {
+    backgroundColor: classList === `box ${value}` ? color : null,
+    border: `1px solid ${color}`,
+    color: color,
+  };
+
+  useEffect(() => {
+    if (boxRef.current) {
+      setClassList(boxRef.current.classList.value);
+    }
+  }, [value]);
+
   return (
-    <>
-    </> 
-  )
+ <div
+  className={`box ${color}`}
+  style={boxCSS}
+  ref={boxRef}
+>{color === value ? <h2 style={{color: 'black'}}>Yes, I'm a {value} color</h2> : <h2>I'm not a {value} color</h2>}
+</div>
+  );
 }
-export default boxColor;  
+
+export default BoxColor;
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,28 @@
-
+import { useRef, useState } from 'react';
+import BoxColor from './BoxColor';
 
 function MyForm() {
+  const [value, setValue] = useState('');
+  const inputRef = useRef(null);
+
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
   return (
-    <></>
+    <>
+      <input
+        type="text"
+        placeholder="Introduce el color"
+        value={value}
+        onChange={() => setValue(inputRef.current?.value || '')}
+        ref={inputRef}
+      />
+
+      <div className="container">
+        {colors.map((color, index) => (
+          <BoxColor key={index} color={color} value={value} />
+        ))}
+      </div>
+    </>
   );
 }
 
